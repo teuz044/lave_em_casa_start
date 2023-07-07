c88bca2c10fe639a6bb756284e2eba7186488027
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2023-07-07 20:04:17 -0300

    Add files via upload

---
+++ api_lave_em_casa/api_lave_em_casa/src/main/java/laveemcasa/api_lave_em_casa/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+//package laveemcasa.api_lave_em_casa.config;
+//
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    //Configurations for authentication
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    }
+//
+//    //Configuration for authorization
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//    }
+//
+//    //Configuration for static resources
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//    }
+//}
---
+++ frontend_lave_em_casa/js/anuncios_criados.js
@@ -0,0 +1,67 @@
+
+// Função para criar um card de anúncio
+function criarCardAnuncio(anuncio) {
+    var card = document.createElement('div');
+    card.classList.add('card-anuncios');
+
+    var titulo = document.createElement('h3');
+    titulo.textContent = anuncio.titulo;
+    card.appendChild(titulo);
+
+    var descricao = document.createElement('p');
+    descricao.textContent = anuncio.descricao, anuncio.proprietarioId;
+    card.appendChild(descricao);
+
+    var valorLavagem = document.createElement('p');
+    valorLavagem.textContent = 'Valor da Lavagem: R$ ' + anuncio.valorLavagem.toFixed(2);
+    card.appendChild(valorLavagem);
+
+
+// Botão de editar
+// var editarButton = document.createElement('button');
+// editarButton.textContent = 'Editar';
+// editarButton.classList.add('btn', 'btn-primary'); // Adicione as classes do Bootstrap
+// editarButton.addEventListener('click', function () {
+//     editarAnuncio(anuncio.proprietarioId, anuncio.id);
+// });
+// card.appendChild(editarButton);
+    
+    return card;
+}
+
+// Função para exibir os anúncios na tela
+function exibirAnuncios(anuncios) {
+    var container = document.getElementById('anunciosContainer');
+
+    anuncios.forEach(function (anuncio) {
+        var card = criarCardAnuncio(anuncio);
+        container.appendChild(card);
+    });
+}
+
+// Buscar os anúncios da API via Fetch
+fetch('http://localhost:8080/anuncios')
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Erro na requisição');
+        }
+    })
+    .then(function (anuncios) {
+        exibirAnuncios(anuncios);
+    })
+    .catch(function (error) {
+        console.error('Erro na requisição:', error);
+    });
+
+// Função para voltar para a tela de criação
+function voltar() {
+    window.location.href = 'index.html';
+}
+
+// Função para editar um anúncio
+function editarAnuncio(proprietarioId, anuncioId) {
+    // Redirecionar para a tela de edição com o ID do anúncio e do proprietário
+    window.location.href = `editar-anuncio.html?proprietarioId=${proprietarioId}&anuncioId=${anuncioId}`;
+}
