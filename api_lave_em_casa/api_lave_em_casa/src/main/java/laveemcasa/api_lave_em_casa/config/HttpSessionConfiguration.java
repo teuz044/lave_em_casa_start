7cdda86658bb8562fbaedcf4cb73d61dbc00fc17
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2023-07-17 21:52:14 -0300

    Delete api_lave_em_casa directory

--- api_lave_em_casa/api_lave_em_casa/src/main/java/laveemcasa/api_lave_em_casa/config/HttpSessionConfiguration.java
+++
@@ -1,9 +0,0 @@
-package laveemcasa.api_lave_em_casa.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-
-@Configuration
-@EnableJdbcHttpSession
-public class HttpSessionConfiguration {
-}
